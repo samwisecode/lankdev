7ddc2eafedbc588e283bdcf105316ccdda1e985e
Sam Norris-Rogers <samnrogers@gmail.com>
2024-08-06 11:00:36 +0200

    Landing page - part 1

--- app.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div class="container mx-auto align-middle text-2xl font-bold">LANK DEV</div>
-</template>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="bg-slate-950 h-screen">
+    <div class="container mx-auto align-middle">
+      <NuxtImg src="/LankDevLogo.svg" width="250" alt="LankDev" />
+      <!-- <NuxtLink to="/about" class="text-white">About</NuxtLink> -->
+    </div>
+    <div class="container mx-auto align-middle mb-4 text-center">
+      <p class="text-white text-4xl uppercase font-extrabold mb-10">
+        Your Vision / Our Code
+      </p>
+      <p class="text-white text-lg">Bring your ideas to the screen.</p>
+    </div>
+    <!-- <div class="container mx-auto align-middle text-center">
+      <button
+        class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+      >
+        Contact Us
+      </button>
+    </div> -->
+  </div>
+</template>
